8c0bad1b9694e198ac5cdee3a145f9d17c412974
Provashish Roy <provashishroy95@gmail.com>
2022-03-22 12:28:26 +0600

    load products data on ui

--- src/components/Header/Shop/Shop.js
+++ src/components/Header/Shop/Shop.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Shop.css';
 
 const Shop = () => {
-
+    const [products, setProducts] = useState([])
+    useEffect(() => {
+        fetch('products.json')
+            .then(res => res.json())
+            .then(data => setProducts(data))
+    }, [])
     return (
         <div className="shop-container">
             <div className="products-container">
-                <h1>This is products container</h1>
+                <h1>This is products container: {products.length}</h1>
             </div>
             <div className="cart-container">
                 <h1>Cart container</h1>
