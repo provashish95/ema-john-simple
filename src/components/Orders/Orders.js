0b560530575728765c8caf41234a529a43927e69
Provashish Roy <provashishroy95@gmail.com>
2022-03-30 21:59:46 +0600

    added custome hooks

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import useProducts from '../../hooks/useProducts';
 import './Orders.css';
 
 const Orders = () => {
+    const [products, setProducts] = useProducts();
+
     return (
         <div>
-            <h1>this is orders page</h1>
+            <h1>this is orders page: {products.length}</h1>
         </div>
     );
 };
 
-export default Orders; <h1>this is orders page</h1>
+export default Orders; 
