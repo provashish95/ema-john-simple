6d401f6dc6400274e6a628e2cc4ceec8b437d6a5
Provashish Roy <provashishroy95@gmail.com>
2022-03-30 22:26:38 +0600

    added custome hooks yseCart

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import useCart from '../../hooks/useCart';
 import useProducts from '../../hooks/useProducts';
 import './Orders.css';
 
 const Orders = () => {
     const [products, setProducts] = useProducts();
+    const [cart, setCart] = useCart(products);
+    console.log(cart);
 
     return (
         <div>
             <h1>this is orders page: {products.length}</h1>
+            <p>Cart has : {cart.length}</p>
         </div>
     );
 };
