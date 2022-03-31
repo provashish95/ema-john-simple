6f246d55b71474e03276ad64cc3196c966a9702d
Provashish Roy <provashishroy95@gmail.com>
2022-03-31 13:21:01 +0600

    uses props of children

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import './Cart.css';
 
-const Cart = ({ cart }) => {
-    //console.log(cart);
+const Cart = (props) => {
+    const { cart } = props;
+    console.log(props.children);
     let total = 0;
     let shipping = 0;
     let quantity = 0;
@@ -23,6 +24,7 @@ const Cart = ({ cart }) => {
             <p>Total Shipping:$ {shipping}</p>
             <p>Tax:$ {tax}</p>
             <h5>Grand Total:$ {grandTotal.toFixed(2)}</h5>
+            {props.children}
         </div>
     );
 };
