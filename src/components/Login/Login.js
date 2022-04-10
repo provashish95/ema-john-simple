99dcc13828c9e4bd827929c07e23731dfd892775
Provashish Roy <provashishroy95@gmail.com>
2022-04-10 17:20:05 +0600

    add some style on login page

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,10 +1,32 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Login.css';
 
 const Login = () => {
     return (
-        <div>
-            <h6>This is login</h6>
+        <div className='form-container'>
+            <div>
+                <h2 className='form-title'>This is login</h2>
+                <form >
+                    <div className="input-group">
+                        <label htmlFor="email">Email</label>
+                        <input type="email" name="email" id="" />
+                    </div>
+                    <div className="input-group">
+                        <label htmlFor="password">Password</label>
+                        <input type="password" name="pass" id="" />
+                    </div>
+                    <input className='form-submit' type="button" value="Login" />
+                </form>
+                <p>
+                    New to ema-john? <Link to='/signup' className='form-link '>Create an account</Link>
+                </p>
+                <div className='form-line'>
+                    <div className='form-line-left'></div>
+                    <div className='form-line-center'>or</div>
+                    <div className='form-line-right'></div>
+                </div>
+            </div>
         </div>
     );
 };
