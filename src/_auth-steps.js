28a972944ce57db7df6b25124b1a0546c1115f95
Provashish Roy <provashishroy95@gmail.com>
2022-04-10 15:59:22 +0600

    added two components

--- src/_auth-steps.js
+++ src/_auth-steps.js
@@ -1,7 +1,8 @@
 /**
  * 1.Create a new project in console.google.com
  * 2. npm install firebase
- * 3.
+ * 3.Create firebase.init.js file and import getAuth to export auth.
+ * 4.Firebase setting > authentication > enable email and password auth
  * 
  * 
  * 
---
+++ src/components/Login/Login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Login.css';
+
+const Login = () => {
+    return (
+        <div>
+            <h6>This is login</h6>
+        </div>
+    );
+};
+
+export default Login;
